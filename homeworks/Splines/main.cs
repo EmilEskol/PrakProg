102dabd286c3f129b1fc7a2f09b1725024708aa0
EmilEskol <emil@svkl.dk>
2023-04-15 10:44:41 +0200

    "a bit more splines"

--- homeworks/Splines/main.cs
+++ homeworks/Splines/main.cs
@@ -1,7 +1,26 @@
 using System;
 using static System.Console;
 class main{
-	static void Main(){
-		Write("hello");	
+	static int Main(string[] args){
+		int returnValue=0,i=0;
+		double[] x,y;
+		string infile=null,outfile=null;
+		foreach(var arg in args){
+			var words=arg.Split(":");
+			if(words[0]=="-input")
+				infile=words[1];
+		}
+		if(infile==null){
+			Error.WriteLine("Wrong inputfilename");
+			returnValue++;
+		}
+		if(returnValue>0)
+			return returnValue;
+		var instream= new System.IO.StreamReader(infile);
+		
+		for(string line=instream.ReadLine();line!=null;line=instream.ReadLine()){
+			(x[i],y[i])=double.Parse(line); //Virker ikke
+		}
+		return returnValue;
 	}
 }
