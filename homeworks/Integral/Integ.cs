e91f910cd5177aae15b4280ede6b92d6c6cfe586
root <root@DESKTOP-GEE3TNR>
2023-04-26 10:02:03 +0200

    "Integral begin"

---
+++ homeworks/Integral/Integ.cs
@@ -0,0 +1,27 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public static class Integ{
+	public static integrate(Func<double,double> f, double a, double b, double delta=0.001,
+		double eps=0.001,double f2=NaN, double f3=NaN){
+		double h=b-a;
+		if(f2==NaN||f3==NaN){
+			f2=(a+2*h/6);
+			f3=(a+4*h/6);
+		}
+		double f1=(a+h/6);
+		double f4=(a+5*h/6);
+		double Q=(2*f1+f2+f3+2*f4)/6*h;
+		double q=(f1+f2+f3+f4)/4*h;
+		double err=Abs(Q-q);
+		if(err<=delta||err<=eps*Abs(Q))
+			return Q;
+		else return integrate(f,a,(a+b)/2,delta/Sqrt(2),eps,f1,f2)+
+					integrate(f,(a+b)/2,b,delta/Sqrt(2),eps,f3,f4);
+	}
+	public static intregrateCC(Func<double,double> f, double a, double b,double delta=0.001,
+		double eps=0.001){
+		return 1;
+	}
+}
