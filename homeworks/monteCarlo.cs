04a159c7a64a57273e8c461c3727845b601d0c3d
EmilEskol <emil@svkl.dk>
2023-05-10 17:58:54 +0200

    "Monte Carlo begin"

---
+++ homeworks/monteCarlo.cs
@@ -0,0 +1,11 @@
+using System;
+using static System.Console;
+
+public static class monteCarlo{
+	public static (double,double) plMonteCarlo(Func<vector,double> f, vector a, vector b, int N){
+
+
+	
+	return result;	
+	}
+}
