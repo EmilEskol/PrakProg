5c30004177d4f52dce5555a10148ee7c6f27d098
EmilEskol <emil@svkl.dk>
2023-05-19 10:01:15 +0200

    "Roots a done"

--- homeworks/Roots/main.cs
+++ homeworks/Roots/main.cs
@@ -7,12 +7,29 @@ using static Roots;
 public class main{
 	public static void Main(string[] args){
 		WriteLine("===========Part a============");
-		Func<vector,vector> f = x => new vector(x[0]*x[0]-2,x[1]*x[1]-4);
-		vector guess=new vector(1,1);
-		vector root=NMBL(f,guess);
-		WriteLine("Finding root in f(x)=x^2-2 from ");
-		guess.print("Guess: ");
-		root.print("A root is at ");
-		f(root).print("And has value ");
+		var rnd = new Random();
+		vector guess,root;
+		Func<vector,vector> f = x => new vector(x[0]*x[0]-1,x[1]*x[1]-4);
+		for(int i=0;i<2;i++){
+			guess=new vector(rnd.NextDouble()*10-5,rnd.NextDouble()*10-5);
+			root=NMBL(f,guess);
+			WriteLine("Finding root in f(x)=x^2-2 from ");
+			guess.print("Guess: ");
+			root.print("A root is at ");
+			f(root).print("And has value ");
+		}
+		WriteLine("Finding extremum of the Rosenbrocks valley function ");
+		WriteLine("The derivative is f'(x)=-2*(1-x)-400*x*(y-x^2),200*(y-x^2)");
+		for(int i=0;i<5;i++){
+			f= x => new vector(-2*(1-x[0])-400*x[0]*(x[1]-x[0]*x[0]),200*(x[1]-x[0]*x[0]));
+			guess=new vector(rnd.NextDouble()*0.5+1,rnd.NextDouble()+2);
+			if(i==1)
+				guess=new vector(-1.34,1.81);
+			guess.print($"Guess {i}: ");
+			root=NMBL(f,guess,Pow(10,-4));
+			root.print("A root is at ");
+			f(root).print("And the derivative has the value ");
+			WriteLine($"the functions value at this point is {Pow((1-root[0]),2)+100*Pow(root[1]-Pow(root[0],2),2)}"); 
+		}
 	}
 }
--- homeworks/Roots/roots.cs
+++ homeworks/Roots/roots.cs
@@ -6,23 +6,26 @@ using static matrix;
 public static class Roots{
 	public static vector NMBL(Func<vector,vector> f, vector x, double eps=1e-2){
 		int dim=x.size;
+		//int i=0;
 		vector delX,x1;
 		x1=x.copy();
-		double val,lambda;
+		double val,lambda,fx1;
 		matrix jacobi,Q,R;
-		f(x1).print("f(x)= ");
-		x1.print("x1= ");
 		do{
+			//i++;
 			jacobi=jacobian(f,x1);
 			(Q,R)=QRGS.decomp(jacobi);
 			delX=QRGS.solve(Q,R,-f(x1));
 			lambda=1;
-			while(f(x1+lambda*delX).norm()>(1-lambda/2)*f(x1).norm() || lambda>Pow(2,-15)){
+			fx1=f(x1).norm();
+			while(f(x1+lambda*delX).norm()>(1-lambda/2)*fx1 &&  lambda>Pow(2,-15)){
 				lambda/=2;
 			}
 			x1 += lambda*delX;
 			val = f(x1).norm();
+			//x1.print("new x guess");
 		}while(val>eps);
+		//WriteLine($"Number of iterations {i}");
 		return x1;
 	}
 	public static matrix jacobian(Func<vector,vector> f, vector x){
