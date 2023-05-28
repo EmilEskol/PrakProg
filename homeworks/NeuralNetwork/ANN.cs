c83b1355b22a911d5557fe1047324a09bcb44697
root <root@DESKTOP-GEE3TNR>
2023-05-28 16:39:22 +0200

    "part a done, b begun"

--- homeworks/NeuralNetwork/ANN.cs
+++ homeworks/NeuralNetwork/ANN.cs
@@ -6,6 +6,9 @@ using static matrix;
 public class ANN{
 	static int n,dim;
 	static Func<double,double> f = x => x*Exp(-x*x);
+	static Func<double,double> inF = x => -0.5*Exp(-x*x);
+	static Func<double,double> deF = x => (1-2*x*x)*Exp(-x*x);
+	static Func<double,double> de2F = x => (2*x*x-3)*2*x*Exp(-x*x);
 	static vector p,x,y;
 	double acc=0.000001;
 	
@@ -21,14 +24,6 @@ public class ANN{
 		ANN.p=p;
 		ANN.n=p.size/3;
 	}
-	public double response(double x){
-		double result=0;
-		for(int i=0;i<ANN.n;i++){
-			int j=i*3;
-			result+=f((x-ANN.p[j])/ANN.p[j+1])*ANN.p[j+2];
-		}
-		return result;
-	}
 	public void train(vector x,vector y){
 		ANN.x=x.copy();
 		ANN.y=y.copy();
@@ -39,7 +34,7 @@ public class ANN{
 		WriteLine($"After trainning the cost function gives {Cost(p)}");
 		p.print(" and p is ");
 	}
-	public static double StResponse(double x){
+	public static double response(double x){
 		double result=0;
 		for(int i=0;i<ANN.n;i++){
 			int j=i*3;
@@ -47,11 +42,35 @@ public class ANN{
 		}
 		return result;
 	}
+	public static double inResponse(double x){
+		double result=0;
+		for(int i=0;i<ANN.n;i++){
+			int j=i*3;
+			result+=inF((x-p[j])/p[j+1])*p[j+2];
+		}
+		return result;
+	}
+	public static double deResponse(double x){
+		double result=0;
+		for(int i=0;i<ANN.n;i++){
+			int j=i*3;
+			result+=deF((x-p[j])/p[j+1])*p[j+2];
+		}
+		return result;
+	}
+	public static double de2Response(double x){
+		double result=0;
+		for(int i=0;i<ANN.n;i++){
+			int j=i*3;
+			result+=de2F((x-p[j])/p[j+1])*p[j+2];
+		}
+		return result;
+	}
 	public static double Cost(vector p){
 		double result=0;
 		ANN.p=p;
 		for(int k=0;k<dim;k++){
-			result+=Pow(StResponse(x[k])-y[k],2);
+			result+=Pow(response(x[k])-y[k],2);
 		}
 		return result;
 	}
--- homeworks/NeuralNetwork/main.cs
+++ homeworks/NeuralNetwork/main.cs
@@ -36,19 +36,15 @@ public class main{
 		y=new vector(nr);
 		y1=new vector(nr);
 		x=new vector(nr);
-		for(int i=0;i<nr;i++){
-			x[i]=10.0/nr*i-5;
-			y[i]=f(x[i]);
-			y1[i]=ANN.StResponse(x[i]);
-
-		}
-		IOputs.WriteXY(args,x,y1,"ANNresPartA1.data");
+		(x,y)=trainData(10.0,1000);
 		IOputs.WriteXY(args,x,y,"valPartA.data");
-
-
-
+		
 		ANN1.train(x,y);
-
+		(x,y)=trainData(4.0,1000);
+		ANN1.train(x,y);
+		(x,y)=trainData(2.0,1000);
+		ANN1.train(x,y);
+		
 		nr=200;
 		y1=new vector(nr);
 		y=new vector(nr);
@@ -56,15 +52,9 @@ public class main{
 		for(int i=0;i<nr;i++){
 			x[i]=8.0/nr*i-4;
 			y[i]=f(x[i]);
-			y1[i]=ANN.StResponse(x[i]);
+			y1[i]=ANN.response(x[i]);
 		}
 		IOputs.WriteXY(args,x,y1,"ANNresPartA.data");
-		p=new vector("0.462 -0.383 1.82 -0.69 0.381 -1.4 -0.0528 1.21 0.315");
-		ANN1 = new ANN(p);
-		for(int i=0;i<nr;i++){
-			y1[i]=ANN.StResponse(x[i]);
-		}
-		IOputs.WriteXY(args,x,y1,"ANNresPartA2.data");
 		
 		
 	}
@@ -72,5 +62,13 @@ public class main{
 		WriteLine("=========Part b=============");
 		WriteLine("Finding derivatives, second derivatives and antiderivative");	
 	}
-	
+	public static (vector x,vector y) trainData(double interval,int nr){
+		y=new vector(nr);
+		x=new vector(nr);
+		for(int i=0;i<nr;i++){
+			x[i]=interval/nr*i-interval/2;
+			y[i]=f(x[i]);
+		}
+		return (x,y);
+	}
 }
