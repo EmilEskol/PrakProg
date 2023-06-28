28f2ebe4a6c7e8f50443904a289fcf479954ec0f
root <root@DESKTOP-GEE3TNR>
2023-06-28 11:39:52 +0200

    "Exam program done but without text"

--- exam/BerrutSpline.cs
+++ exam/BerrutSpline.cs
@@ -1,80 +1,100 @@
 using System;
+using System.Threading;
 using static System.Console;
 using static System.Math;
+using static vector;
 public class BerrutSpline{
-	vector x,y;
+	public class data{
+		public int a,b;
+		public double z,numerab,denomab;
+		public double[] xs,ys;
+	}
+	
+	vector x;
 	double numer,denom;//numerater and denominator
-	int n;
-	public BerrutSpline(vector xs,vector ys){
+	int n,nThreads;
+	data[] locs;
+	
+	public BerrutSpline(string[] args, vector xs,vector ys){
 		n=xs.size;
 		x=xs.copy();
-		y=ys.copy();
+		WriteLine($"Size of vectors {n}");
+		foreach(var arg in args){
+			var words = arg.Split(':');
+			if(words[0]=="-threads") 
+				nThreads = (int)float.Parse(words[1]);
+		}
+		WriteLine($"N# of threads {nThreads} and N# data points {n}");
+		locs = new data[nThreads];
+		for(int i=0;i<nThreads;i++){
+			locs[i] = new data();
+			locs[i].a = (i*n)/nThreads;
+			locs[i].b = ((i+1)*n)/nThreads;
+			locs[i].xs=new double[locs[i].b-locs[i].a];
+			locs[i].ys=new double[locs[i].b-locs[i].a];
+			for(int j=locs[i].a;j<locs[i].b;j++){
+				locs[i].xs[j-locs[i].a] = xs[j];
+				locs[i].ys[j-locs[i].a] = ys[j];
+			}
+			WriteLine($"Thread number{i} has a={locs[i].a} b={locs[i].b}");
+		}
 	}
 	public double evaluate(double z){
 		numer=0;
 		denom=0;
-		//WriteLine($"z={z}");
-		for(int i=0;i<n;i++){
+		for(int i=0;i<nThreads;i++){
+			for(int j=0;j<locs[i].xs.Length;j++){
+				if(z==locs[i].xs[j])
+					return locs[i].ys[j];
+			}
+			locs[i].z=z;
+		}
+		Thread[] threads = new Thread[nThreads];
+		for(int i=0;i<nThreads;i++){
+			threads[i] = new Thread(Sum);
+			threads[i].Start(locs[i]);
+		}
+		for(int i=0;i<nThreads;i++){
+			threads[i].Join();
+		}
+		for(int i=0;i<nThreads;i++){
+			numer+=locs[i].numerab;
+			denom+=locs[i].denomab;
+		}
+
+		/*for(int i=0;i<n;i++){
 			if(z==x[i])
 				return y[i];
 			numer+=Pow(-1,i)/(z-x[i])*y[i];
 			denom+=Pow(-1,i)/(z-x[i]);
-			//WriteLine($"i={i},numerator={numer}, demoninator={denom}");
-		}
-		//WriteLine($"val={numer/denom}");
+		}*/
+		//WriteLine($"z {z} nomer {numer} denom {denom}");
 		return numer/denom;
+	}
+	public void Sum(object obj){
+		data loc= (data) obj;
+		double x;
+		loc.numerab=0;
+		loc.denomab=0;
+		for(int i=0;i<loc.b-loc.a;i++){
+			x=loc.xs[i];
+			loc.numerab+=Pow(-1,i)/(loc.z-x)*loc.ys[i];
+			loc.denomab+=Pow(-1,i)/(loc.z-x);
+		}
 	}
 	public (double[],double[]) printData(int m){
 			int size=(n-1)*m;
-			double step;
+			double step,x1,x2;
 			double[] xData = new double[size];
 			double[] yData = new double[size];
 			for(int i=0;i<n-1;i++){
 				for(int j=0;j<m;j++){
-					step=(j-1)*(x[i+1]-x[i])/m;
-					xData[m*i+j]=x[i]+step;
-					yData[m*i+j]=evaluate(xData[m*i+j]);
+					x1=x[i];x2=x[i+1];
+					step=(j-1)*(x2-x1)/m;
+					xData[m*i+j]=x1+step;
+					yData[m*i+j]=evaluate(x1+step);
 				}
 			}
 		return (xData,yData);
 	}
-	/*public double derivative(double z){
-		int i=Spline.binSearch(x,z);
-		return b[i]+2*c[i]*(z-x[i]);
-	}*/
-	/*public double integral(double z){
-		int i=Spline.binSearch(x,z);
-		if(i==0)
-			return  y[i]*(z-x[i])+0.5*b[i]*Pow(z-x[i],2)+1/3*c[i]*Pow(z-x[i],3);
-		double k=integral(x[i]-0.00000001);
-		return  k+y[i]*(z-x[i])+0.5*b[i]*Pow(z-x[i],2)+1/3*c[i]*Pow(z-x[i],3);
-	}*/
-	/*public (double[],double[]) printDerivative(int m){
-			int size=(n-1)*m;
-			double step;
-			double[] xData = new double[size];
-			double[] yData = new double[size];
-			for(int i=0;i<n-1;i++){
-				for(int j=0;j<m;j++){
-					step=j*(x[i+1]-x[i])/m;
-					xData[m*i+j]=x[i]+step;
-					yData[m*i+j]=b[i]+2*c[i]*(step-x[i]);
-				}
-			}
-		return (xData,yData);
-	}*/
-	/*public (double[],double[]) printIntegral(int m){
-			int size=(n-1)*m;
-			double step;
-			double[] xData = new double[size];
-			double[] yData = new double[size];
-			for(int i=0;i<n-1;i++){
-				for(int j=0;j<m;j++){
-					step=j*(x[i+1]-x[i])/m;
-					xData[m*i+j]=x[i]+step;
-					yData[m*i+j]=integral(x[i]+step);
-				}
-			}
-		return (xData,yData);
-	}*/
 }
--- exam/main.cs
+++ exam/main.cs
@@ -7,8 +7,25 @@ using static vector;
 class main{
 	static int Main(string[] args){
 		int returnValue=0;
-
-		//Berut spline
+		bool time=false;
+		foreach(var arg in args){
+			var words = arg.Split(':');
+			if(words[0]=="-time")
+				if(words[1]=="true")
+					time=true;
+		}
+		if(!time){
+			partA(args);
+			partB1(args);
+			partB2(args);
+		}
+		else
+			partC(args);
+		
+		return returnValue;
+	}
+	public static void partA(string[] args){
+	//Berut spline
 		vector x= new vector(10);
 		vector y1= new vector(10);
 		vector y2 = new vector(10);
@@ -20,13 +37,12 @@ class main{
 		
 		
 		WriteLine("Data for Berrutspline:");
-		x.print("x:");
-		y2.print("y:");
 		
-		BerrutSpline berrut= new BerrutSpline(x,y1);
-		BerrutSpline berrut2= new BerrutSpline(x,y2);
-		
+		BerrutSpline berrut= new BerrutSpline(args,x,y1);
+		BerrutSpline berrut2= new BerrutSpline(args,x,y2);
+
 		//Making a plot with berut
+		WriteLine("Making a plot with berut");
 		(double[] xs,double[] ys)=berrut.printData(100);
 		(double[] xs2,double[] ys2)=berrut2.printData(100);
 
@@ -35,19 +51,21 @@ class main{
 		
 		IOputs.WriteXY(args,x,y1,"berrutData.data");
 		IOputs.WriteXY(args,x,y2,"berrutData2.data");
-
+	}
+	public static void partB1(string[] args){
 		//Testing different methods to make berut spline converge
+		WriteLine("Testing different methods to make berut spline converge");
 		vector x1= new vector(10);
 		vector x2= new vector(10);
 		vector x3= new vector(30);
 		vector x4= new vector(30);
+		vector y1= new vector(10);
+		vector y2= new vector(10);
 		vector y3= new vector(30);
 		vector y4= new vector(30);
 		
 		for(int j=0;j<x3.size;j++){
-			WriteLine($"j{j} {x3.size}");
 			if(j<x1.size){
-				WriteLine("j>x2.size");
 				x1[j]=j;
 				x2[j]=3*j;
 				y1[j]=Cos(x1[j]);
@@ -60,32 +78,53 @@ class main{
 		}
 		IOputs.WriteXY(args,x4,y4,"berrutDataCos.data");
 		
-		BerrutSpline berrut3= new BerrutSpline(x1,y1);
+		BerrutSpline berrut3= new BerrutSpline(args,x1,y1);
 		(double[] xs3,double[] ys3)=berrut3.printData(100);
 		IOputs.WriteXY(args,xs3,ys3,"berrutSplineCos.data");
 
-		berrut3= new BerrutSpline(x2,y2);
+		berrut3= new BerrutSpline(args,x2,y2);
 		(xs3, ys3)=berrut3.printData(100);
 		IOputs.WriteXY(args,xs3,ys3,"berrutSplineCos2.data");
 
-		berrut3= new BerrutSpline(x3,y3);
+		berrut3= new BerrutSpline(args,x3,y3);
 		(xs3,ys3)=berrut3.printData(100);
 		IOputs.WriteXY(args,xs3,ys3,"berrutSplineCos3.data");
 
-		berrut3= new BerrutSpline(x4,y4);
+		berrut3= new BerrutSpline(args,x4,y4);
 		(xs3,ys3)=berrut3.printData(100);
 		IOputs.WriteXY(args,xs3,ys3,"berrutSplineCos4.data");
-		
-		
-		/*	Testing convergence of different functions with using a broader band 
-			and larger amounts of numbers*/	
-		Conver(args,100,1000);
-
-		
-		return returnValue;
 	}
-	public static void Conver(string[] args,int step,int max){
+	public static void partB2(string[] args){
+		/*	Testing convergence of different functions with using a broader band 
+			and larger amounts of numbers*/
+		WriteLine("Testing convergence of different functions with using a broader band\nand larger amounts of numbers");	
+		Func<double,double> f= x=>Cos(x);
+		Conver(args,f,"Convergence.data",10,50);
+		f= x=> Exp(-x*x);
+		Conver(args,f,"Convergence2.data",10,50);
+		f= x=> x*x;
+		Conver(args,f,"Convergence3.data",10,50);
+		f= x=> 2*x*x;
+		Conver(args,f,"Convergence4.data",10,50);
+	
+		
+	}
+	public static void partC(string[] args){
+		int nPoints=2000000;
+		int nEvals=10000;
+		double[] x= new double[nPoints];
+		double[] y= new double[nPoints];
+		for(int i=0;i<nPoints;i++){
+			x[i]=i*1.0/nPoints-0.5;
+			y[i]=Exp(-x[i]*x[i]);
+		}
+		BerrutSpline berrut= new BerrutSpline(args,x,y);
+		for(int i=0;i>nEvals;i++)
+			berrut.evaluate(i*0.8/nEvals-0.4);		
+	}
+	public static void Conver(string[] args, Func<double,double> f,string filename,int step,int max){
 		double diff=0;
+		int nPoints=100;
 		genlist<double> diffs= new genlist<double>();
 		genlist<double> nData= new genlist<double>();
 		vector x,y;
@@ -94,19 +133,21 @@ class main{
 			y= new vector(i);
 			for(int j=0;j<x.size;j++){
 				x[j]=j;
-				y[j]=Cos(x[j]);
+				y[j]=f(x[j]);
 			}
-
-			BerrutSpline berrut= new BerrutSpline(x,y);
-			(double[] xs,double[] ys)=berrut.printData(500);
-			
+			WriteLine($"step {i}");
+			BerrutSpline berrut= new BerrutSpline(args,x,y);
+			(double[] xs,double[] ys)=berrut.printData(nPoints);
+			/*for(int j=0;j<500;j++){
+				WriteLine($"{xs[j]}	{ys[j]}");
+			}*/
 			diff=0;
-			for(int j=0;j<500;j++){
-				diff+=Pow(ys[j]-Cos(xs[j]),2);
+			for(int j=0;j<nPoints;j++){
+				diff+=Pow(ys[j]-f(xs[j]),2)/nPoints;
 			}
 			nData.add(i);
 			diffs.add(diff);
 		}
-		IOputs.WriteXY(args,nData,diffs,"Convergence.data");
+		IOputs.WriteXY(args,nData,diffs,filename);
 	}
 }
